7183053740bf39b3069145c39d500e8cb2e9920f
Rudmila5 <rudmila138@gmail.com>
2025-11-01 21:27:49 -0400

    Quick Fix

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.use(cors({
 app.use(express.json());
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'monorail.proxy.rlwy.net',
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'yiIqbIUFsCFBshHtqlFyEraZAOcqKaly',
-  database: process.env.MYSQL_DATABASE || 'railway',
-  port: process.env.MYSQL_PORT || 21049,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
   connectionLimit: 10,
   connectTimeout: 30000
 });
